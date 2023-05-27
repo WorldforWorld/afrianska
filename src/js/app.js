e14eaef705de10c1aa43cbef075c9ed894ccb846
Yuri Kuzmichev <48134058+WorldforWorld@users.noreply.github.com>
2023-05-27 14:01:59 +0500

    create fomr submit

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,23 @@
 import * as nav from './modules/nav.js';
 import * as scroll from './modules/scroll-header.js';
 import { isWebp } from './modules/functions.js';
+import { popup } from './modules/form_popup.js';
+import { handleSubmit } from './modules/handleSubmit.js';
 isWebp();
 document.addEventListener('DOMContentLoaded', function () {
   // Скролл для шапки
   scroll.addClassScroll();
   window.addEventListener('scroll', scroll.addClassScroll);
-
   // Бургер меню
   nav.burgerMenu();
+	// Форма обратной связи
+	popup()
+
+	const form = document.querySelector('form');
+
+
+	form.addEventListener('submit', (event) => {
+		handleSubmit(event, form);
+	});
+	
 });
---
+++ src/js/modules/handleSubmit.js
@@ -0,0 +1,67 @@
+const validateField = (field) => {
+	if (field.value.trim() === '') {
+		return false;
+	}
+
+	if (field.id === 'user-name') {
+		const nameRegex = /^[а-яА-ЯёЁa-zA-Z]{3,8}$/;;
+		return nameRegex.test(field.value.trim());
+	}
+
+	if (field.id === 'user-email') {
+		const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+		return emailRegex.test(field.value.trim());
+	}
+
+	return true;
+};
+const message = (form, text, fields) => {
+	form.classList.remove('load');
+	form.parentNode.parentNode.parentNode.classList.add('success');
+	const textMessage = form.parentNode.parentNode.parentNode.querySelector('.form__message span');
+	textMessage.textContent = text;
+	setTimeout(() => {
+		form.parentNode.parentNode.parentNode.classList.remove('open');
+	}, 1000);
+	setTimeout(() => {
+		form.parentNode.parentNode.parentNode.classList.remove('success');
+		textMessage.textContent = '';
+	}, 1500);
+	fields.forEach((field) => {
+		field.value = '';
+	})
+}
+export function handleSubmit (event, form) {
+	event.preventDefault();
+
+	let isValid = true;
+	const fields = form.querySelectorAll('.form__input');
+
+	fields.forEach((field) => {
+		const isValidField = validateField(field);
+		const formGroup = field.closest('.form__group');
+
+		if (!isValidField) {
+			isValid = false;
+			formGroup.classList.add('error');
+		} else {
+			formGroup.classList.remove('error');
+		}
+	});
+
+	if (isValid) {
+		const formData = new FormData(form);
+		form.classList.add('load')
+		fetch('https://jsonplaceholder.typicode.com/posts', {
+			method: 'POST',
+			body: formData
+		})
+		.then(response => response.json())
+		.then(data => {
+			message(form, 'Your message successfully sent', fields);
+		})
+		.catch(error => {
+				message(form, error, fields);
+			});
+		}
+};
