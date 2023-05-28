d00c6a2a59b006ce70599295443de039f4211cdf
Yuri Kuzmichev <48134058+WorldforWorld@users.noreply.github.com>
2023-05-28 17:30:20 +0500

    fix code

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,13 @@
 import * as nav from "./modules/nav.js";
-import * as scroll from "./modules/scroll-header.js";
 import { isWebp } from "./modules/functions.js";
 import { popup } from "./modules/form_popup.js";
 import { handleSubmit } from "./modules/handleSubmit.js";
 
 isWebp();
 document.addEventListener("DOMContentLoaded", function () {
-  // Скролл для шапки
-  scroll.addClassScroll();
-  window.addEventListener("scroll", scroll.addClassScroll);
   // Бургер меню
   nav.burgerMenu();
+
   // Форма обратной связи
   popup();
 
