063df486bc85bdae153388d1af8d8a1aec998b77
Yuri Kuzmichev <48134058+WorldforWorld@users.noreply.github.com>
2023-05-28 17:31:35 +0500

    remove script

--- src/js/modules/scroll-header.js
+++
@@ -1,8 +0,0 @@
-export function addClassScroll() {
-  const header = document.querySelector("header");
-  if (window.pageYOffset > 100) {
-    header.classList.add("scroll");
-  } else {
-    header.classList.remove("scroll");
-  }
-}
