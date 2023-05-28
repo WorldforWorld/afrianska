7ff623338da048f8d8d10e9092d9b690d1571033
Yuri Kuzmichev <48134058+WorldforWorld@users.noreply.github.com>
2023-05-28 17:32:35 +0500

    add scroll animation for menu

--- src/js/modules/nav.js
+++ src/js/modules/nav.js
@@ -9,10 +9,39 @@ export function burgerMenu() {
     body.classList.toggle("lock-js");
   });
   menuLinks.forEach((link) => {
-    link.addEventListener("click", () => {
-      burgerMenu.classList.toggle("active");
-      mainMenu.classList.toggle("active");
-      body.classList.toggle("lock-js");
+    link.addEventListener("click", (event) => {
+      burgerMenu.classList.remove("active");
+      mainMenu.classList.remove("active");
+      body.classList.remove("lock-js");
+      addClassScroll(event);
     });
   });
+}
+
+function addClassScroll(event) {
+  event.preventDefault();
+  const id = event.target.getAttribute("href");
+  const section = document.querySelector(id);
+  const top = section.offsetTop;
+  const duration = 1000;
+  const start = window.pageYOffset;
+  const distance = top - start;
+  let startTime = null;
+
+  function animation(currentTime) {
+    if (startTime === null) startTime = currentTime;
+    const timeElapsed = currentTime - startTime;
+    const run = ease(timeElapsed, start, distance, duration);
+    window.scrollTo(0, run);
+    if (timeElapsed < duration) requestAnimationFrame(animation);
+  }
+
+  function ease(t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) return (c / 2) * t * t + b;
+    t--;
+    return (-c / 2) * (t * (t - 2) - 1) + b;
+  }
+
+  requestAnimationFrame(animation);
 }
