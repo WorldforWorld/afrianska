c19397da2d5af928454526f007d9e811bdd69b6e
Yuri Kuzmichev <48134058+WorldforWorld@users.noreply.github.com>
2023-05-28 17:32:06 +0500

    add manipulation class lock-js in body

--- src/js/modules/form_popup.js
+++ src/js/modules/form_popup.js
@@ -3,13 +3,16 @@ export function popup() {
   const formShadow = document.querySelector(".form__shadow");
   const formClose = document.querySelector(".form__close");
   const openPopup = document.querySelector('a.btn.btn__primary[href="#popup"]');
+  const body = document.querySelector("body");
 
   function openForm() {
     formShadow.classList.add("open");
+    body.classList.add("lock-js");
   }
 
   function closeForm() {
     formShadow.classList.remove("open");
+    body.classList.remove("lock-js");
   }
 
   openPopup.addEventListener("click", openForm);
