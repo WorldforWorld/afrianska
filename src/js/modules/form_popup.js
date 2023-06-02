da8cec7d80b5f3154c59c5285fc32c993c1dbe40
Юрий Кузмичев <kuzmichev@it-tian.ru>
2023-06-02 12:22:32 +0500

    change for openPopup and add check for addEventListener

--- src/js/modules/form_popup.js
+++ src/js/modules/form_popup.js
@@ -2,22 +2,25 @@ export function popup() {
   // Получаем ссылки на элементы формы
   const formShadow = document.querySelector(".form__shadow");
   const formClose = document.querySelector(".form__close");
-  const openPopup = document.querySelector('a.btn.btn__primary[href="#popup"]');
+  const openPopup = document.querySelector('a[data-target="feedback-popup"]');
   const body = document.querySelector("body");
+  const shadow = document.querySelector(".shadow");
 
   function openForm() {
     formShadow.classList.add("open");
     body.classList.add("lock-js");
+    shadow.classList.add("active");
   }
 
   function closeForm() {
     formShadow.classList.remove("open");
     body.classList.remove("lock-js");
+    shadow.classList.remove("active");
   }
 
-  openPopup.addEventListener("click", openForm);
-  formClose.addEventListener("click", closeForm);
-  formShadow.addEventListener("click", function (event) {
+  openPopup?.addEventListener("click", openForm);
+  formClose?.addEventListener("click", closeForm);
+  formShadow?.addEventListener("click", function (event) {
     if (event.target === formShadow) {
       closeForm();
     }
